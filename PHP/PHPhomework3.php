3d07487e0d33c928c91b34785f842d057af5cc69
iida-takuya <takuya.iida@tsukuba.pascalia.co.jp>
2021-04-27 11:17:08 +0900

    Update PHPhomework

--- PHP/PHPhomework3.php
+++ PHP/PHPhomework3.php
@@ -1,17 +1,18 @@
 <?php
 $target = 10;
-
 $flg = true;
-for($i=2;$i<$target;$i++) {
-	  if($target%$i == 0) {
-		      $flg = false;
-		          break;
-		        }
+
+for ($i=2;$i<$target;$i++) {
+	    if ($target%$i == 0) {
+		            $flg = false;
+			            break;
+			        }
 }
 
 if ($flg) {
-	  echo $target. " is a prime number.";
+	    echo $target. " is a prime number";
 } else {
-	  echo $target. " is not a prime number";
+	    echo $target. " is not a prime number";
 }
+
 ?>
