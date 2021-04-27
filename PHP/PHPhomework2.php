4704da7d86a832d4efea13750a57382f343d4247
iida-takuya <82427718+iida-takuya@users.noreply.github.com>
2021-04-27 13:04:51 +0900

    Update PHPhomework2.php

--- PHP/PHPhomework2.php
+++ PHP/PHPhomework2.php
@@ -1,7 +1,7 @@
 <?php
 function factorialOfNumber ($n) {
-    if ($n>0) {
-        return $n *= factorialOfNumber ($n-1);
+    if ($n > 0) {
+        return $n *= factorialOfNumber ($n - 1);
     }
     return 1;
 }
