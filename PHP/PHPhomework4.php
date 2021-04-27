121394bff565f980c67fdb59e96bb69a2af54f81
iida-takuya <82427718+iida-takuya@users.noreply.github.com>
2021-04-27 13:20:23 +0900

    Update PHPhomework4.php

--- PHP/PHPhomework4.php
+++ PHP/PHPhomework4.php
@@ -3,16 +3,16 @@
 $array = array();
 array_push($array,2);
 
-for ($i = 3;$i <= 100;$i++) {
+for ($i = 3; $i <= 100; $i++) {
     $k = true;
-    for ($j = 2;$j < $i;$j++) {
-        if ($i % $j == 0) {
-            $k = false;
+        for ($j = 2; $j < $i; $j++) {
+            if ($i % $j == 0) {
+                $k = false;
+            }
         }
-    }
-    if ($k == true) {
-        array_push($array,$i);
-    }
+        if ($k == true) {
+            array_push($array,$i);
+        }
 }
 
 foreach ($array as $val) {
