34c2a863c206be4d162f77e002cd3d614df69639
iida-takuya <82427718+iida-takuya@users.noreply.github.com>
2021-04-28 16:38:45 +0900

    Update PHPhomework4.php

--- PHP/PHPhomework4.php
+++ PHP/PHPhomework4.php
@@ -2,19 +2,19 @@
 // reuse the function at step 3
 function isPrimeNumber($n) {
     // Because 0 and 1 are not prime numbers
-    if ($n == 0 || $n == 1) {
+    if ($n === 0 || $n === 1) {
         return false;
     }
 
     // 2 is a prime number
-    if ($n == 2) {
+    if ($n === 2) {
         return true;
     }
 
     $m = $n/2;
     for ($i = 2; $i <= $m; $i++) {
         // if can devided by another number (not 1 and itself), not prime number
-        if ($n%$i == 0) {
+        if ($n%$i === 0) {
             return false;
         }
     }
