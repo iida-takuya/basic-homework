3a3d944d5d69f2422fd2937ad137f58067d2861a
iida-takuya <82427718+iida-takuya@users.noreply.github.com>
2021-04-28 16:32:03 +0900

    Update PHPhomework1.php

--- PHP/PHPhomework1.php
+++ PHP/PHPhomework1.php
@@ -3,5 +3,5 @@ $sum = 0;
 for ($i = 1; $i <= 50; $i = $i + 2) {
     $sum = $sum + $i;
 }
-echo "The sum of odd numbers from 0 to 50 = ".$sum."<br>";
+echo 'The sum of odd numbers from 0 to 50 = ' . $sum . '<br/>';
 ?>
