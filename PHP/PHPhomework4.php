1146c56bce69f1d5050d1a21e3df7953da9f913b
iida-takuya <82427718+iida-takuya@users.noreply.github.com>
2021-04-28 09:12:58 +0900

    Update PHPhomework4.php

--- PHP/PHPhomework4.php
+++ PHP/PHPhomework4.php
@@ -1,22 +1,39 @@
 <?php
-//0～100
-$array = array();
-array_push($array,2);
+// reuse the function at step 3
+function isPrimeNumber($n) {
+    // Because 0 and 1 are not prime numbers
+    if ($n == 0 || $n == 1) {
+        return false;
+    }
 
-for ($i = 3; $i <= 100; $i++) {
-    $k = true;
-        for ($j = 2; $j < $i; $j++) {
-            if ($i % $j == 0) {
-                $k = false;
-            }
+    // 2 is a prime number
+    if ($n == 2) {
+        return true;
+    }
+
+    $m = $n/2;
+    for ($i = 2; $i <= $m; $i++) {
+        // if can devided by another number (not 1 and itself), not prime number
+        if ($n%$i == 0) {
+            return false;
         }
-        if ($k == true) {
-            array_push($array,$i);
-        }
+    }
+
+    return true;
 }
 
-foreach ($array as $val) {
-echo ($val . "\n");
+function getPrimeNumbers($tn) {
+    $result = [];
+    for ($i = 1; $i <= $tn; $i++) {
+        if (isPrimeNumber($i)) {
+            array_push($result, $i);
+        }
+    }
+
+    return $result;
 }
+
+// Try to check the function
+print_r(getPrimeNumbers(10));
 
 ?>
