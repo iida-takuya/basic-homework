0ae9ca9e1333099d21f9f1c60d3cd25037dc6f09
iida-takuya <takuya.iida@tsukuba.pascalia.co.jp>
2021-04-30 09:33:01 +0900

    php-homework

--- PHP/PHPhomework3.php
+++ PHP/PHPhomework3.php
@@ -1,18 +1,37 @@
 <?php
-$target = 10;
-$flg = true;
+function isPrimeNumber($n) {
+    // Because 0 and 1 are not prime numbers
+    if ($n === 0 || $n === 1) {
+        return false;
+    }
 
-for ($i=2;$i<$target;$i++) {
-	    if ($target%$i == 0) {
-		            $flg = false;
-			            break;
-			        }
+    // 2 is a prime number
+    if ($n === 2) {
+        return true;
+    }
+
+    $m = $n/2;
+    for ($i = 2; $i <= $m; $i++) {
+        // if can devided by another number (not 1 and itself), not prime number
+        if ($n % $i === 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
-if ($flg) {
-	    echo $target. " is a prime number";
+// Try to check a number is prime number or not
+if (isPrimeNumber(7)) {
+    echo "7 is prime number\n";   
 } else {
-	    echo $target. " is not a prime number";
+    echo "7 is not prime number\n"; 
+}
+
+if (isPrimeNumber(6)) {
+    echo "6 is prime number\n";   
+} else {
+    echo "6 is not prime number\n"; 
 }
 
 ?>
