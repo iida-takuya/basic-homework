5212e1da719220b4b053c3b026ce900d913da881
iida-takuya <takuya.iida@tsukuba.pascalia.co.jp>
2021-06-11 17:14:33 +0900

    HTMLCSSJS-homework

--- HTMLCSSJS/HTMLCSSJShomework1.js
+++ HTMLCSSJS/HTMLCSSJShomework1.js
@@ -1,19 +1,40 @@
-function scrollToTop() {
-    window.scrollTo({top: 0, behavior: 'smooth'});
-}
+window.addEventListener('load', () => {
+    toggleScrollButton();
+});
 
-function showLoginForm() {
+document.addEventListener('mouseup', (e) => {
     const form = document.getElementById('login');
     if (!form) { return; }
-    form.style.display = form.style.display ? '' : 'block';
+    if (!form.contains(e.target) && !e.target.innerText.toLowerCase().includes('login')) {
+        form.style.display = '';
+    }
+});
+
+function scrollToTop() {
+    window.scrollTo({top: 0, behavior: 'smooth'}); 
 }
 
-window.addEventListener('scroll', function() {
-    const currentScrollPosition = window.scrollY;
+function toggleScrollButton() {
+    const lastPosition = window.scrollY;
     const scrollBtn = document.getElementById('scroll');
-    if (currentScrollPosition !== 0) {
+    
+    if (lastPosition !== 0) {
         scrollBtn.setAttribute('style', 'display: block;');
     } else {
         scrollBtn.setAttribute('style', 'display: none;');
     }
+}
+
+window.addEventListener('scroll', function() {
+    toggleScrollButton();
 });
+
+function showLoginForm() {
+    const form = document.getElementById('login');
+    
+    if (!form) { return; }
+    
+    form.style.display = form.style.display ? '' : 'block';
+}
+
+
